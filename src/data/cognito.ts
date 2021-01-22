bffb9d34573e6ea1969171bcc200fa371db9edf2
matt-twomey <matt.twomey@infinityworks.com>
2021-01-22 16:56:57 +0000

    Implemented simple add user functionality with form reset

--- src/data/cognito.ts
+++ src/data/cognito.ts
@@ -1,12 +1,19 @@
 import { CognitoIdentityServiceProvider } from 'aws-sdk';
 import { Auth } from 'aws-amplify';
 import {
+    AdminCreateUserRequest,
     ListUserPoolsRequest,
     ListUsersRequest,
     UserPoolDescriptionType,
     UserType,
 } from 'aws-sdk/clients/cognitoidentityserviceprovider';
-import { AWS_REGION } from '../constants';
+import { AWS_REGION, MAIN_USER_POOL_PREFIX } from '../constants';
+import { FormUser } from '../pages/AddUser';
+
+interface CognitoClientAndUserPool {
+    client: CognitoIdentityServiceProvider;
+    userPoolId: string | undefined;
+}
 
 export const getCognitoClient = async (): Promise<CognitoIdentityServiceProvider> =>
     new CognitoIdentityServiceProvider({ region: AWS_REGION, credentials: await Auth.currentUserCredentials() });
@@ -17,6 +24,13 @@ export const getUserPoolList = async (cognito: CognitoIdentityServiceProvider): 
     const listUserPoolsResponse = await cognito.listUserPools(params).promise();
 
     return listUserPoolsResponse?.UserPools ?? [];
+};
+
+export const getCognitoClientAndUserPool = async (): Promise<CognitoClientAndUserPool> => {
+    const client = await getCognitoClient();
+    const userPoolList = await getUserPoolList(client);
+    const userPool = userPoolList?.find((pool) => pool.Name?.startsWith(MAIN_USER_POOL_PREFIX));
+    return { client, userPoolId: userPool?.Id };
 };
 
 export const listUsersInPool = async (
@@ -30,4 +44,22 @@ export const listUsersInPool = async (
     const listUsersResponse = await cognito.listUsers(params).promise();
 
     return listUsersResponse?.Users ?? [];
+};
+
+const generateTemporaryPassword = (): string =>
+    Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+
+export const addUserToPool = async (
+    cognito: CognitoIdentityServiceProvider,
+    userPoolId: string,
+    formUser: FormUser,
+): Promise<void> => {
+    const params: AdminCreateUserRequest = {
+        UserPoolId: userPoolId,
+        Username: formUser.email,
+        UserAttributes: [{ Name: 'custom:noc', Value: formUser.nocs }],
+        TemporaryPassword: generateTemporaryPassword(),
+    };
+
+    await cognito.adminCreateUser(params).promise();
 };
--- src/pages/AddUser.tsx
+++ src/pages/AddUser.tsx
@@ -1,6 +1,54 @@
 import { H1 } from '@govuk-react/heading';
 import { ReactElement } from 'react';
+import Button from '@govuk-react/button';
+import { useForm } from 'react-hook-form';
+import { addUserToPool, getCognitoClientAndUserPool } from '../data/cognito';
 
-const AddUser = (): ReactElement => <H1>Add User</H1>;
+export interface FormUser {
+    email: string;
+    nocs: string;
+}
+
+const formatNocs = (nocs: string): string =>
+    nocs
+        .split(',')
+        .map((noc) => noc.trim())
+        .join('|');
+
+const AddUser = (): ReactElement => {
+    const { register, handleSubmit, reset } = useForm<FormUser>();
+    const onSubmit = async (formUser: FormUser) => {
+        const formattedUser = { ...formUser, nocs: formatNocs(formUser.nocs) };
+        const { client, userPoolId } = await getCognitoClientAndUserPool();
+        if (userPoolId) {
+            const response = await addUserToPool(client, userPoolId, formattedUser);
+            console.log(response);
+        }
+        reset();
+    };
+
+    return (
+        <>
+            <H1>Add User</H1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="email">User Email</label>
+                <br />
+                <input id="email" name="email" ref={register({ required: true })} />
+                <br />
+                <br />
+                <label htmlFor="email">User National Operator Code (NOC)</label>
+                <br />
+                <small>
+                    If the user has multiple NOCs, enter a comma-separated list. For example, &apos;NOC1,NOC2,NOC3&apos;
+                </small>
+                <br />
+                <input id="nocs" name="nocs" ref={register({ required: true })} />
+                <br />
+                <br />
+                <Button>Submit</Button>
+            </form>
+        </>
+    );
+};
 
 export default AddUser;
